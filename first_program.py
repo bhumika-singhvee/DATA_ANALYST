2797b35426500be7c6ce504d442f6425e7685f1b
bhumika-singhvee <138039560+bhumika-singhvee@users.noreply.github.com>
2024-08-10 15:09:57 +0530

    BASICS OF PYTHON QUESTION SOLVING

---
+++ first_program.py
@@ -0,0 +1,25 @@
+"""name=input("enter your name")
+age=int(input("enter your age"))
+address=input("enter your address")
+print(name)
+print(age)
+print(address)
+##################################
+#swap aa variable
+a=45
+b=15
+c=a
+a=b
+b=c
+print(a)
+print(b)
+#method2
+a=30
+b=40
+a,b=b,a
+print(a,b)
+#####################
+#convert a float into integer
+x=12.5
+x=int(x)
+print(type(x),x)"""
